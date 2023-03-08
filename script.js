52ae6dee9c0f7880176986532bcc5d672f5afa74
Felix/failaxite <failaxite.don@gmail.com>
2023-03-08 08:11:11 +0100

    :sparkles: Adding other pages for testing api

---
+++ script.js
@@ -0,0 +1,9 @@
+var map = new google.maps.StreetViewPanorama(
+    document.getElementById('map'),
+    {
+      position: {lat: 48.753830, lng: 2.296390},
+      pov: {heading: 165, pitch: 0},
+      zoom: 1
+    }
+  );
+  
